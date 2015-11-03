dfba7769cc2e2a1a0c2d5aec443f86e4b62a21e6
Manos Fountoulakis <manos@figmints.com>
2015-11-03 13:56:34 -0500

    comment undeleted scope

--- app/models/spree/recurring.rb
+++ app/models/spree/recurring.rb
@@ -12,7 +12,7 @@ module Spree
     validates :type, :name, presence: true
     validates :type, uniqueness: { message: 'of provider recurring already exists' }
 
-    scope :active, -> { undeleted.where(active: true) }
+    # scope :active, -> { undeleted.where(active: true) }
 
     def self.display_name
       name.gsub(%r{.+:}, '')
